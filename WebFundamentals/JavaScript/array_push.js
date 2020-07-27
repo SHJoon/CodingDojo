8ceb53bb09b9c9b8b08d6bdb49e0323276458c9b
SHJoon <52848941+SHJoon@users.noreply.github.com>
2020-07-27 11:21:11 -0700

    Finished Loops Predict Assignment

---
+++ WebFundamentals/JavaScript/array_push.js
@@ -0,0 +1,23 @@
+/*
+1.) Recreate the push function.
+Add a new val to back of an array passed in without using .push
+*/
+
+// example = [0,1,2,3];
+// Output: example = [0,1,2,3,4]; WITHOUT using .push method
+
+
+
+
+
+
+
+
+
+/* 
+  Array: Push Front
+  Given an array and an additional value, insert this value at the beginning of the array. Do this without using any built-in array methods.
+*/
+
+// example = [0,1,2,3];
+// Output: example = [4,0,1,2,3];
