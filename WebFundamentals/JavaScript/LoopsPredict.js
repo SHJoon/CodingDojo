1a73a522f883455dbc8c67758fd5ed127d6c524f
SHJoon <52848941+SHJoon@users.noreply.github.com>
2020-07-27 12:32:53 -0700

    Finished Loops Algo Assignement

--- WebFundamentals/JavaScript/LoopsPredict.js
+++ WebFundamentals/JavaScript/LoopsPredict.js
@@ -1,6 +1,6 @@
 // Predict 1
-for(var num = 0; num < 15; num++){
-    console.log(num);
+for (var num = 0; num < 15; num++) {
+  console.log(num);
 }
 // Output
 // 0
@@ -20,26 +20,24 @@ for(var num = 0; num < 15; num++){
 // 14
 
 // Predict 2
-for(var i = 1; i < 10; i+=2){
-    if(i % 3 == 0){
-        console.log(i);
-    }
+for (var i = 1; i < 10; i += 2) {
+  if (i % 3 == 0) {
+    console.log(i);
+  }
 }
 // Output
 // 3
 // 6
 // 9
 
-
 // Predict 3
-for(var j = 1; j <= 15; j++){
-    if(j % 2 == 0){
-        j+=2;
-    }
-    else if(j % 3 == 0){
-        j++;
-    }
-    console.log(j);
+for (var j = 1; j <= 15; j++) {
+  if (j % 2 == 0) {
+    j += 2;
+  } else if (j % 3 == 0) {
+    j++;
+  }
+  console.log(j);
 }
 
 // Output
