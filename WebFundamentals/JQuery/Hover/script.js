4ab62820b955ee23c1bba2cea6ed9ce199e905fb
SHJoon <52848941+SHJoon@users.noreply.github.com>
2020-07-30 15:46:38 -0700

    Finished hover assignment

---
+++ WebFundamentals/JQuery/Hover/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    
+    $("img").hover(function(){
+        $(this).attr("temp", $(this).attr("src"));
+        $(this).attr("src", $(this).attr("other-src"));
+        $(this).attr("other-src", $(this).attr("temp"));
+        $(this).removeAttr("temp")
+    }, function(){
+        $(this).attr("temp", $(this).attr("src"));
+        $(this).attr("src", $(this).attr("other-src"));
+        $(this).attr("other-src", $(this).attr("temp"));
+        $(this).removeAttr("temp")
+    })
+
+
+});
