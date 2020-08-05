8f94f89477a52157fc14d948131da744841926f5
SHJoon <52848941+SHJoon@users.noreply.github.com>
2020-08-05 13:27:55 -0700

    Understanding Routes assignemnt

---
+++ python_stack/flask/flask_fundamentals/understanding_routing/understanding_flask.py
@@ -0,0 +1,32 @@
+from flask import Flask
+
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return 'Hello World!'  # Return the string 'Hello World!' as a response
+
+@app.route('/dojo')
+def dojo():
+  return "Dojo"
+
+@app.route('/say/<name>')
+def say(name):
+    print(name)
+    return "Hi " + name.capitalize() + "!"
+
+@app.route('/repeat/<num>/<word>')
+def repeat(num, word):
+    myStr = ""
+    for i in range(int(num)):
+        myStr += word + " "
+    return myStr
+
+@app.route('/users/<username>/<id>') # for a route '/users/____/____', two parameters in the url get passed as username and id
+def show_user_profile(username, id):
+    print(username)
+    print(id)
+    return "username: " + username + ", id: " + id
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
