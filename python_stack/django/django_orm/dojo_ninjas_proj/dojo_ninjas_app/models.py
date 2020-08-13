473bdfdab71f49a4f408aac55535d25dddf3a052
SHJoon <52848941+SHJoon@users.noreply.github.com>
2020-08-12 19:42:22 -0700

    Finished DojoNinja Shell assignment

--- python_stack/django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/models.py
+++ python_stack/django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/models.py
@@ -5,6 +5,7 @@ class Dojo(models.Model):
     name = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
+    desc = models.TextField(null=True)
 
 class Ninja(models.Model):
     first_name = models.CharField(max_length=255)
@@ -12,6 +13,6 @@ class Ninja(models.Model):
 
     dojo_id = models.ForeignKey(
         Dojo,
-        related_name = "ninja",
+        related_name = "ninjas",
         on_delete = models.CASCADE
     )
