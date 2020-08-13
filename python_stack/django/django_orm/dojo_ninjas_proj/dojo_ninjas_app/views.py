8c51ca355de5f32f4e2f40b8b6a59bca0a4b9771
SHJoon <52848941+SHJoon@users.noreply.github.com>
2020-08-13 01:07:52 -0700

    Finished Dojos & Ninjas Template assignment

--- python_stack/django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/views.py
+++ python_stack/django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Dojo, Ninja
 
 # Create your views here.
+def index(request):
+    context={
+        "all_dojos": Dojo.objects.all(),
+    }
+    return render(request, "index.html", context)
+
+def add_dojo(request):
+    Dojo.objects.create(
+        name = request.POST["name"],
+        city = request.POST["city"],
+        state = request.POST["state"],
+    )
+    return redirect("/")
+
+def add_ninja(request):
+    Ninja.objects.create(
+        first_name = request.POST["first_name"],
+        last_name = request.POST["last_name"],
+        dojo_id = Dojo.objects.get(id=request.POST["dojo-name"]),
+    )
+    return redirect("/")
+
+def delete(request):
+    Dojo.objects.get(id=request.POST["delete"]).delete()
+
+    return redirect("/")
