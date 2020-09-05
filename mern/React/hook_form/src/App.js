f83b530fe659f3fc4a9f5acacaf01d2bd82e6359
SHJoon <52848941+SHJoon@users.noreply.github.com>
2020-09-05 14:13:23 -0700

    More forms assignment

--- mern/React/hook_form/src/App.js
+++ mern/React/hook_form/src/App.js
@@ -2,21 +2,12 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Forms from './components/Forms';
-import Results from './components/Results';
 
 function App() {
-  const [state, setState] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    confirmPassword: ""
-  });
 
   return (
     <div className="App">
-      <Forms inputs={state} setInputs={setState} />
-      <Results data={state} />
+      <Forms />
     </div>
   );
 }
