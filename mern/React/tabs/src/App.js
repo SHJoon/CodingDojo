0a2f180939af6e90ef4951190b40bc629b4a7872
SHJoon <52848941+SHJoon@users.noreply.github.com>
2020-09-06 03:13:04 -0700

    Tabs assignment starting

---
+++ mern/React/tabs/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Tab from './components/Tab';
+
+function App() {
+  const tabs = [
+    {
+      label:
+    },
+  ]
+
+  return (
+    <div className="App">
+      <div>
+        <Tab />
+        <Tab />
+        <Tab />
+      </div>
+      <div>
+        <p>Content is gonna go here</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ mern/React/tabs/src/components/Tab.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const Tab = (props) => {
+
+    return(
+        <div>
+
+        </div>
+    );
+}
