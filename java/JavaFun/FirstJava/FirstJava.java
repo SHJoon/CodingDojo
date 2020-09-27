f3606a302b6a26ddbf805b6dc9faef8288d6ad2e
SHJoon <52848941+SHJoon@users.noreply.github.com>
2020-09-26 21:29:16 -0700

    Completed first java assignment

---
+++ java/JavaFun/FirstJava/FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My name is Howard Shin");
+        System.out.println("I am 24 years old");
+        System.out.println("My hometown is Chatsworth, CA");
+    }
+}
