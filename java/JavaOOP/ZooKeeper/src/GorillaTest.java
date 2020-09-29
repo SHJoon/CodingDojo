8bb40e60be32345226bb30ff4e75f591d1a71aff
SHJoon <52848941+SHJoon@users.noreply.github.com>
2020-09-28 19:35:03 -0700

    Finished Zoo Keeper Part 1

---
+++ java/JavaOOP/ZooKeeper/src/GorillaTest.java
@@ -0,0 +1,19 @@
+
+public class GorillaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gorilla gorilla = new Gorilla();
+		
+		gorilla.throwSomething();
+		gorilla.throwSomething();
+		gorilla.throwSomething();
+		
+		gorilla.eatBananas();
+		gorilla.eatBananas();
+		gorilla.displayEnergy();
+		gorilla.climb();
+		gorilla.displayEnergy();
+	}
+
+}
---
+++ java/JavaOOP/ZooKeeper/src/Mammal.java
@@ -0,0 +1,9 @@
+
+public class Mammal {
+	public int energyLevel;
+	
+	public int displayEnergy() {
+		System.out.println(energyLevel);
+		return energyLevel;
+	}
+}
