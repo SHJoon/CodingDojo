eaeefa3f9e825fc3d7d9bb3249127737cd6c3493
SHJoon <52848941+SHJoon@users.noreply.github.com>
2020-09-30 13:48:05 -0700

    Finished calculator 1 assignment

--- java/JavaOOP/Calculator/src/com/howarshin/calculator/Calculator.java
+++ java/JavaOOP/Calculator/src/com/howarshin/calculator/Calculator.java
@@ -1,10 +1,45 @@
 package com.howarshin.calculator;
 
 public class Calculator implements java.io.Serializable {
+	private double operandOne;
+	private String operation;
+	private double operandTwo;
+	private double results;
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public double getOperandOne() {
+		return operandOne;
 	}
-
+	public void setOperandOne(double operandOne) {
+		this.operandOne = operandOne;
+	}
+	public String getOperation() {
+		return operation;
+	}
+	public void setOperation(String operation) {
+		this.operation = operation;
+	}
+	public double getOperandTwo() {
+		return operandTwo;
+	}
+	public void setOperandTwo(double operandTwo) {
+		this.operandTwo = operandTwo;
+	}
+	public double getResults() {
+		return results;
+	}
+	public void setResults(double results) {
+		this.results = results;
+	}
+	
+	public void performOperation(){
+		if (getOperation() == "+") {
+			setResults(getOperandOne() + getOperandTwo());
+		}
+		else if (getOperation() == "-") {
+			setResults(getOperandOne() - getOperandTwo());
+		}
+		else {
+			System.out.println("Unrecognized operation: " + getOperation());
+		}
+	}
 }
