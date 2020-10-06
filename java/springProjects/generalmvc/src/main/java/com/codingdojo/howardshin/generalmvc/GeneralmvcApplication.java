ab107aba6de3fe8f35e444f382e27b92a21cbd62
SHJoon <52848941+SHJoon@users.noreply.github.com>
2020-10-06 00:16:15 -0700

    Finished update/delete

---
+++ java/springProjects/generalmvc/src/main/java/com/codingdojo/howardshin/generalmvc/GeneralmvcApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.howardshin.generalmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralmvcApplication.class, args);
+	}
+
+}
---
+++ java/springProjects/generalmvc/src/test/java/com/codingdojo/howardshin/generalmvc/GeneralmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.howardshin.generalmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
