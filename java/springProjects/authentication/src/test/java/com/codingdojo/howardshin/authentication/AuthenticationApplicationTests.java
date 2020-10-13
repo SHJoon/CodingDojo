86fd8cc7283a5787d42e142386142cb174d36f0f
SHJoon <52848941+SHJoon@users.noreply.github.com>
2020-10-12 22:03:33 -0700

    Finished Controllers and Views assignment

---
+++ java/springProjects/authentication/src/test/java/com/codingdojo/howardshin/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.howardshin.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
